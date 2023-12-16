6a9d6c969980245966ecd448ee92cf1b827ec265
drushikachanda <chandadrushika6@gmail.com>
2023-12-16 19:42:16 +0530

    Add project files.

---
+++ BlogDAL/BlogDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogDAL
+{
+    public class BlogDbContext : DbContext
+    {
+        public BlogDbContext() :base("name=BlogTrackerConnection")
+        {
+            Database.SetInitializer(new DbInitializer());
+        }
+        public DbSet<AdminInfo> AdminInfos { get; set; }
+        public DbSet<EmpInfo> EmpInfos { get; set; }
+      
+
+        public DbSet<BlogInfo> BlogInfos { get; set; }
+    }
+
+}
---
+++ BlogDAL/DbInitializer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogDAL
+{
+    public class DbInitializer : DropCreateDatabaseIfModelChanges<BlogDbContext>
+    {
+        protected override void Seed(BlogDbContext context)
+        {
+            Console.WriteLine("DbInitializer Seed method is executing.");
+            // Seed default data for AdminInfo table
+            var admin = new AdminInfo
+            {
+                EmailId = "drushika@admin.com",
+                Password = "Drushika@1234"
+            };
+            context.AdminInfos.Add(admin);
+
+            // Add additional seeding for other tables if needed
+
+            base.Seed(context);
+        }
+
+    }
+}
