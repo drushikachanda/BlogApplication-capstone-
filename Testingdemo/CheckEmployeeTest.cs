ac4accb714f676455b7c841dd3fb78794bcc50a3
drushikachanda <chandadrushika6@gmail.com>
2023-12-18 16:41:23 +0530

    first changes

---
+++ Testingdemo/CheckEmployeeTest.cs
@@ -0,0 +1,24 @@
+﻿using BlogDAL;
+using BlogDAL.Repository;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testingdemo
+{
+    [TestFixture]
+    public class CheckEmployeeTest
+    {
+        [Test]
+        public void checkEmployeeExist()
+        {
+            var obj = new EmpRepository(new BlogDbContext());
+            var res = obj.GetEmpInfoByEmialId("user2@example.com");
+            Assert.That(res, Is.Not.Null);
+        }
+
+    }
+}
